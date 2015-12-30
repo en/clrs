233f2d007f985530176506c971a2bc6a360b3b35
Yuanchao Sun <yuanchao.sun@gmail.com>
2015-12-30 14:52:44 +0800

    recursive linear search

--- linear_search.go
+++ linear_search.go
@@ -1,10 +1,21 @@
 package clrs
 
-func linearSearch(a []int, v int) int {
+func iterativeLinearSearch(a []int, v int) int {
 	for i := 0; i <= len(a)-1; i++ {
 		if a[i] == v {
 			return i
 		}
 	}
 	return -1
+}
+
+func recursiveLinearSearch(a []int, v, n int) int {
+	if a[n] == v {
+		return n
+	}
+	if n == 0 {
+		return -1
+	} else {
+		return recursiveLinearSearch(a, v, n-1)
+	}
 }
--- linear_search_test.go
+++ linear_search_test.go
@@ -4,9 +4,20 @@ import (
 	"testing"
 )
 
-func TestLinearSearch(t *testing.T) {
+func TestIterativeLinearSearch(t *testing.T) {
 	for _, c := range searchCases {
-		got := linearSearch(c.a, c.v)
+		got := iterativeLinearSearch(c.a, c.v)
+		if got != c.want {
+			t.Errorf("search for %v in %v", c.v, c.a)
+			t.Errorf("got %v", got)
+			t.Errorf("want %v", c.want)
+		}
+	}
+}
+
+func TestRecursiveLinearSearch(t *testing.T) {
+	for _, c := range searchCases {
+		got := recursiveLinearSearch(c.a, c.v, len(c.a)-1)
 		if got != c.want {
 			t.Errorf("search for %v in %v", c.v, c.a)
 			t.Errorf("got %v", got)
