bcdb522d9776c732b0a278414dddd026dceddcc1
Yuanchao Sun <yuanchao.sun@gmail.com>
2016-08-05 16:11:10 +0800

    trying to group test cases together ...

--- src/ch02/insertion_sort.rs
+++ src/ch02/insertion_sort.rs
@@ -31,57 +31,34 @@ pub fn recursive_insertion_sort<T: cmp::PartialOrd + Copy>(a: &mut [T], n: i32) 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use ::test_cases;
 
     #[test]
     fn iterative() {
-        let mut a = [5, 2, 4, 6, 1, 3];
-        iterative_insertion_sort(&mut a);
-        assert_eq!([1, 2, 3, 4, 5, 6], a);
-
-        let mut a = ['y', 'u', 'a', 'n', 'c', 'h', 'a', 'o'];
-        iterative_insertion_sort(&mut a);
-        assert_eq!(['a', 'a', 'c', 'h', 'n', 'o', 'u', 'y'], a);
-
-        let mut a: [i32; 0] = [];
-        iterative_insertion_sort(&mut a);
-        let want: [i32; 0] = [];
-        assert_eq!(want, a);
-
-        let mut a = [1];
-        iterative_insertion_sort(&mut a);
-        assert_eq!([1], a);
-
-        let mut a = [31, 41, 59, 26, 41, 58];
-        iterative_insertion_sort(&mut a);
-        assert_eq!([26, 31, 41, 41, 58, 59], a);
+        macro_rules! iterative {
+            ( $( $i:tt ),* ) => {{
+                $(
+                    let (mut a, want) = test_cases::SORT.$i;
+                    iterative_insertion_sort(&mut a);
+                    assert_eq!(want, a);
+                )*
+            }};
+        }
+        iterative!(0, 1, 2, 3, 4, 5, 6, 7);
     }
 
     #[test]
     fn recursive() {
-        let mut a = [5, 2, 4, 6, 1, 3];
-        let l = a.len() as i32;
-        recursive_insertion_sort(&mut a, l - 1);
-        assert_eq!([1, 2, 3, 4, 5, 6], a);
-
-        let mut a = ['y', 'u', 'a', 'n', 'c', 'h', 'a', 'o'];
-        let l = a.len() as i32;
-        recursive_insertion_sort(&mut a, l - 1);
-        assert_eq!(['a', 'a', 'c', 'h', 'n', 'o', 'u', 'y'], a);
-
-        let mut a: [i32; 0] = [];
-        let l = a.len() as i32;
-        recursive_insertion_sort(&mut a, l - 1);
-        let want: [i32; 0] = [];
-        assert_eq!(want, a);
-
-        let mut a = [1];
-        let l = a.len() as i32;
-        recursive_insertion_sort(&mut a, l - 1);
-        assert_eq!([1], a);
-
-        let mut a = [31, 41, 59, 26, 41, 58];
-        let l = a.len() as i32;
-        recursive_insertion_sort(&mut a, l - 1);
-        assert_eq!([26, 31, 41, 41, 58, 59], a);
+        macro_rules! recursive {
+            ( $( $i:tt ),* ) => {{
+                $(
+                    let (mut a, want) = test_cases::SORT.$i;
+                    let l = a.len() as i32;
+                    recursive_insertion_sort(&mut a, l - 1);
+                    assert_eq!(want, a);
+                )*
+            }};
+        }
+        recursive!(0, 1, 2, 3, 4, 5, 6, 7);
     }
 }
